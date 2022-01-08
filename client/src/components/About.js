10fb97dd37f1f2d163fe218b41083bb3dcd2d13c
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2022-01-08 16:48:35 -0500

    about page style and responsiveness updates

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -14,9 +14,9 @@ const About = () => {
 
         <Container className='container text-dark d-flex flex-wrap justify-content-evenly mt-4 about' >
             
-            <Card border='danger' style={{ width: '30rem' }} className='shadow p-3 mb-5 bg-body rounded about'>
+            <Card border='danger' style={{ width: '30rem' }} className='shadow mb-5 bg-body rounded about'>
                 <Card.Img className='p-3' variant="top" src={EC} />
-                <Card.Header>History</Card.Header>
+                <Card.Header className='bg-dark text-light text-center'>HISTORY</Card.Header>
                 <Card.Text className='p-3'>
                     <LoremIpsum
                     avgWordsPerSentence={5}
@@ -26,9 +26,9 @@ const About = () => {
                 </Card.Text>
             </Card>
 
-            <Card border='danger' style={{ width: '30rem' }} className='shadow p-3 mb-5 bg-body rounded about'>
+            <Card border='danger' style={{ width: '30rem' }} className='shadow mb-5 bg-body rounded about'>
                 <Card.Img className='p-3' variant="top" src={NY} />
-                <Card.Header>Mission</Card.Header>
+                <Card.Header className='bg-dark text-light text-center' >MISSION</Card.Header>
                 <Card.Text className='p-3'>
                     <LoremIpsum
                     avgWordsPerSentence={5}
@@ -42,8 +42,8 @@ const About = () => {
     
         <h2 className='text-center mt-4'>OUR OFFICERS</h2>
 
-        <Container className='d-flex flex-wrap justify-content-evenly volunteers'>
-            <Card className='ms-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
+        <Container className='d-flex flex-wrap justify-content-center align-items-center volunteers'>
+            <Card className='ms-5 me-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
                 <Card.Header>President</Card.Header>  
                 <Card.Img src='https://cdn.stocksnap.io/img-thumbs/960w/person-portrait_NDKEEMX5WP.jpg' style={{ height: '15rem'}}></Card.Img>
                 <Card.Body className='bg-dark text-light'>
@@ -55,7 +55,7 @@ const About = () => {
                 <Card.Footer>District</Card.Footer>
             </Card>
             <br />
-            <Card className='ms-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
+            <Card className='ms-5 me-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
                 <Card.Header>Vice President</Card.Header>    
                 <Card.Img src='https://cdn.stocksnap.io/img-thumbs/960w/woman-natural_X9QBBG5DZF.jpg' style={{ height: '15rem'}}></Card.Img>
                 <Card.Body className='bg-dark text-light'>
@@ -67,7 +67,7 @@ const About = () => {
                 <Card.Footer>District</Card.Footer>
             </Card>
             <br />
-            <Card className='ms-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
+            <Card className='ms-5 me-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
                 <Card.Header>2nd Vice President</Card.Header>
                 <Card.Img src='https://cdn.stocksnap.io/img-thumbs/960w/deer-nature_YVQMPLACTF.jpg' style={{ height: '15rem'}}></Card.Img>
                 <Card.Body className='bg-dark text-light'>
@@ -79,7 +79,7 @@ const About = () => {
                 <Card.Footer>District</Card.Footer>
             </Card>
             <br />
-            <Card className='ms-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
+            <Card className='ms-5 me-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
                 <Card.Header>Secretary</Card.Header>
                 <Card.Img src='https://cdn.stocksnap.io/img-thumbs/960w/boy-face_IBGY6G6AWT.jpg' style={{ height: '15rem'}}></Card.Img>
                 <Card.Body className='bg-dark text-light'>
@@ -91,7 +91,7 @@ const About = () => {
                 <Card.Footer>District</Card.Footer>
             </Card>
             <br />
-            <Card className='ms-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
+            <Card className='ms-5 me-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
                 <Card.Header>Treasurer</Card.Header>
                 <Card.Img src='https://cdn.stocksnap.io/img-thumbs/960w/business-man_7BQNRHB6EX.jpg' style={{ height: '15rem'}}></Card.Img>
                 <Card.Body className='bg-dark text-light'>
@@ -103,7 +103,7 @@ const About = () => {
                 <Card.Footer>District</Card.Footer>
             </Card>
             <br />
-            <Card className='ms-5 mt-3 mb-5 officer' border="dark" style={{ width: '20rem' }}>
+            <Card className='ms-5 me-5 mt-auto mb-5 officer' border="dark" style={{ width: '20rem' }}>
                 <Card.Header>Sergeant-at-Arms</Card.Header>
                 <Card.Img src='https://cdn.stocksnap.io/img-thumbs/960w/squirrel-nature_XT2APRFPRO.jpg' style={{ height: '15rem'}}></Card.Img>
                 <Card.Body className='bg-dark text-light'>
